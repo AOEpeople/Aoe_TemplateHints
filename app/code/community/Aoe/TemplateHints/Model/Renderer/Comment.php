7387c88bfaa2063bad20853dbed515d258e81ad5
Johannes Klein <jo@temka.de>
2014-11-26 19:03:53 +0100

    Update Comment.php
    fixes #24 and also adds missing contend from nested arrays

--- app/code/community/Aoe/TemplateHints/Model/Renderer/Comment.php
+++ app/code/community/Aoe/TemplateHints/Model/Renderer/Comment.php
@@ -52,23 +52,44 @@ class Aoe_TemplateHints_Model_Renderer_Comment extends Aoe_TemplateHints_Model_R
         $helper = Mage::helper('aoe_templatehints/blockInfo'); /* @var $helper Aoe_TemplateHints_Helper_BlockInfo */
 
         $output = '';
-
-        foreach ($info as $label => $value) {
-
-            if (in_array($label, array('name', 'alias'))) {
-                continue;
+        
+        $output .= $this->arrayToTabList($info, array('name', 'alias'));   
+        
+        if (count($path) > 0) { 
+            $output .= "\t" . $helper->__('Block nesting').":\n";
+            foreach ($path as $step) {
+                $output .= "\t\t" . $helper->renderTitle($step) . "\n";
             }
-            $output .= "\t" . ucfirst($label) . ":\n";
-            $output .= "\t\t$value\n";
         }
-
-        $output .= "\t" . $helper->__('Block nesting').":\n";
-        foreach ($path as $step) {
-            $output .= "\t\t" . $helper->renderTitle($step) . "\n";
-        }
-
+        
         return $output;
     }
-
-
+    
+    protected function arrayToTabList(array $array, array $skipKeys=array(), $nestingTabs = "\t") {
+        $output = '';
+        foreach ($array as $key => $value) {
+            if (in_array($key, $skipKeys, true)) {
+                continue;
+            }
+            if (is_array($value)) {
+                $nestingTabs .= "\t";
+                $value = $this->arrayToTabList($value, $skipKeys, $nestingTabs);
+                if (strpos($value, "\t\t") < 2) {
+                    $value = substr($value, 2);
+                }
+                else {
+                    $value = substr($value, 1);
+                }
+                $nestingTabs = substr($nestingTabs, -1);
+            }
+            if (is_int($key)) {
+                $output .= "$nestingTabs\t$value\n";
+            } else {
+                $output .= "$nestingTabs" . ucfirst($key) . ":\n";
+                $output .= "$nestingTabs\t$value\n";
+            }
+    
+        }
+        return $output;
+    }
 }
